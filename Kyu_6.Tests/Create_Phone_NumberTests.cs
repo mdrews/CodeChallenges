2b3be15ead9c110dd8dff6427560f6439fc57c6c
mdrews <matthewdrews@gmail.com>
2018-09-05 23:04:09 -0400

    Adding Kyu_6 and Phone Number problem

---
+++ Kyu_6.Tests/Create_Phone_NumberTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kyu_6.Tests
+{
+    public class Create_Phone_NumberTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            string expected = "(123) 456-7890";
+            string actual = Create_Phone_Number.CreatePhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Kyu_6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kyu_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
