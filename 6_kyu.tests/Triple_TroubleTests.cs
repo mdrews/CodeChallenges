606c93fc5a271dbb2eb5a81f5eadb97da448ab0d
mdrews <matthewdrews@gmail.com>
2018-09-08 19:55:07 -0400

    Adding 6Kyu - Create Phone Numbers and Triple Trouble

---
+++ 6_kyu.tests/Triple_TroubleTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kyu_6.Tests
+{
+    public class Triple_TroubleTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            int expected = 1;
+            int actual = Triple_Trouble.TripleDouble(451999277, 41177722899);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Test2()
+        {
+            int expected = 0;
+            int actual = Triple_Trouble.TripleDouble(1222345, 12345);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Test3()
+        {
+            int expected = 0;
+            int actual = Triple_Trouble.TripleDouble(12345, 12345);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Test4()
+        {
+            int expected = 1;
+            int actual = Triple_Trouble.TripleDouble(666789, 12345667);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Test5()
+        {
+            int expected = 1;
+            int actual = Triple_Trouble.TripleDouble(10560002, 100);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            int expected = 0;
+            int actual = Triple_Trouble.TripleDouble(594421776, 534215177);
+            Assert.Equal(expected,actual);
+        }
+    }
+}
---
+++ 6_kyu/Triple_Trouble.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Kyu_6
+{
+    public class Triple_Trouble
+    {
+        public static int TripleDouble(long num1, long num2)
+        {
+            string strNum1 = num1.ToString();
+            string strNum2 = num2.ToString();
+            List<char> numbers = new List<char>();
+            char current = ' ';
+            int count = 0;
+            for (int x = 0; x < strNum1.Length; x++)
+            {
+                if (x == 0)
+                {
+                    current = strNum1[0];
+                    count++;
+                }
+                else
+                {
+                    if (strNum1[x] == current)
+                    {
+                        count++;
+                        if (count == 3)
+                        {
+                            numbers.Add(current);
+                        }
+                    }
+                    else
+                    {
+                        count = 1;
+                        current = strNum1[x];
+                    }
+                }
+            }
+            count = 1;
+            if (numbers.Count == 0)
+            {
+                return 0;
+            }
+            for (int y = 0; y < strNum2.Length; y++)
+            {
+                if (y == 0)
+                {
+                    current = strNum2[0];
+                }
+                else
+                {
+                    if (strNum2[y] == current && numbers.Contains(current))
+                    {
+                        count++;
+                        if (count == 2)
+                        {
+                            return 1;
+                        }
+                    }
+                    else
+                    {
+                        count = 1;
+                        current = strNum2[y];
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
