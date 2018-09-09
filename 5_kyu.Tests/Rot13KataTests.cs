7a35bc5ccd9f9880f7786ed4a45c5a6ddab338fe
mdrews <matthewdrews@gmail.com>
2018-09-09 10:39:35 -0400

    Adding Rot13 (5kyu)

---
+++ 5_kyu.Tests/Rot13KataTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace _5_kyu.Tests
+{ 
+    public class Rot13KataTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            string expected = "grfg";
+            string actual = Rot13Kata.Rot13("test");
+            Assert.Equal(expected,actual);
+        }
+        [Fact]
+        public void Test2()
+        {
+            string expected = "Grfg";
+            string actual = Rot13Kata.Rot13("Test");
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ 5_kyu/Rot13Kata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_kyu
+{
+    public class Rot13Kata
+    {
+        public static string Rot13(string message)
+        {
+            for (int x = 0; x < message.Length; x++)
+            {
+                char current = message[x];
+                if ((current > 64 && current < 78) || current > 96 && current < 110)
+                {
+                    current = (char)(Convert.ToInt16(current) + 13);
+                }
+                else if ((current > 77 && current < 91) || current > 109 && current < 123)
+                {
+                    current = (char)(Convert.ToInt16(current) - 13);
+                }
+                message = message.Remove(x, 1).Insert(x, current.ToString());
+            }
+            return message;
+        }
+    }
+}
