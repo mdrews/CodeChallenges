0fa0420c37862385551d8b324cc2d17323b746d6
mdrews <matthewdrews@gmail.com>
2020-05-18 23:53:58 -0400

    update to include jest

--- CodingGame/practice/medium/rock-paper-scissors-war-test.js
+++ CodingGame/practice/medium/rock-paper-scissors-war-test.js
@@ -1,20 +1,7 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+const jest = require('jest');
 
-var inputs = readline().split(' ');
-const w = parseInt(inputs[0]);
-const h = parseInt(inputs[1]);
-const n = parseInt(inputs[2]);
-
-let map = [];
-for (let i = 0; i < h; i++) {
-    const line = readline();
-    map = [...map, line];
-}
-
-// Write an answer using console.log()
-// To debug: console.error('Debug messages...');
-
-console.log(map);
+describe('test case 1', () => {
+    it('should return correct result', () => {
+        return null;
+    });
+});
