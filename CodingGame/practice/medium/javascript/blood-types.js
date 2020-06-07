055e6d35d1a0974907d88cac437ccb32eb0a33ab
mdrews <matthewdrews@gmail.com>
2020-06-06 23:57:42 -0400

    add puzzle of the week (blood-types) to codingame

---
+++ CodingGame/practice/medium/javascript/blood-types.js
@@ -0,0 +1,17 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const N = parseInt(readline());
+for (let i = 0; i < N; i++) {
+    var inputs = readline().split(' ');
+    const parent1 = inputs[0];
+    const parent2 = inputs[1];
+    const child = inputs[2];
+}
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
