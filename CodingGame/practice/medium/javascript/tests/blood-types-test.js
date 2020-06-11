9cded0fb23cc17a567ac602ceee5bc707781e4b3
mdrews <matthewdrews@gmail.com>
2020-06-10 23:57:04 -0400

    add Child blood types test to Codingame->blood-types challenge (javascript)

--- CodingGame/practice/medium/javascript/tests/blood-types-test.js
+++ CodingGame/practice/medium/javascript/tests/blood-types-test.js
@@ -16,3 +16,36 @@ describe('Example', () => {
     expect(bloodTypes(input)).toEqual(output);
   });
 });
+
+describe('Child Blood Types', () => {
+  test('it should return output given input', () => {
+    const input = `
+      12
+      AB+ B- ?
+      A+ O- ?
+      O+ AB+ ?
+      A- A+ ?
+      O+ B- ?
+      O- AB- ?
+      AB- A+ ?
+      AB+ AB+ ?
+      A+ A- ?
+      A- B- ?
+      B- O- ?
+      O- O+ ?`;
+    const output = `
+      A+ A- AB+ AB- B+ B-
+      A+ A- O+ O-
+      A+ A- B+ B-
+      A+ A- O+ O-
+      B+ B- O+ O-
+      A- B-
+      A+ A- AB+ AB- B+ B-
+      A+ A- AB+ AB- B+ B-
+      A+ A- O+ O-
+      A- AB- B- O-
+      B- O-
+      O+ O-`;
+      expect(bloodTypes(input)).toEqual(output);
+  });
+});
