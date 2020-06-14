19616c763ffce3a16f521e9c6f4ef4d192efc535
mdrews <matthewdrews@gmail.com>
2020-06-13 23:50:36 -0400

    add last test case to blood-types-test

--- CodingGame/practice/medium/javascript/tests/blood-types-test.js
+++ CodingGame/practice/medium/javascript/tests/blood-types-test.js
@@ -121,3 +121,110 @@ const output = `
       expect(bloodTypes(input).toEqual(output));
   });
 });
+
+describe('All combins', () => {
+  test('it should return defined output given the input', () => {
+    const input = `50
+      ? AB- B-
+      AB- O+ ?
+      O- ? B-
+      B- A+ ?
+      B- ? B+
+      O- ? B+
+      O- A+ ?
+      ? O+ A-
+      AB- ? O+
+      B- ? B-
+      ? B+ AB-
+      ? B+ AB+
+      ? O- O+
+      A+ O+ ?
+      ? O- O-
+      O+ AB- ?
+      ? A- A+
+      ? A- A-
+      B+ AB- ?
+      A+ AB- ?
+      ? A+ O-
+      ? B+ B-
+      ? A+ O+
+      ? A+ AB+
+      O- B+ ?
+      ? A+ AB-
+      A- A- ?
+      ? AB+ O+
+      O+ B+ ?
+      ? O+ AB-
+      O- AB+ ?
+      ? B+ B+
+      ? AB+ O-
+      AB+ A- ?
+      ? O+ AB+
+      ? AB- A+
+      AB- A- ?
+      ? A- AB+
+      AB+ AB- ?
+      A+ ? B+
+      A+ ? B-
+      B+ ? A+
+      ? B+ A+
+      AB+ ? O+
+      ? B+ A-
+      A- B+ ?
+      B- O+ ?
+      B+ B- ?
+      AB+ ? O-
+      B+ ? A-`;
+const output = `A+ A- AB+ AB- B+ B- O+ O-
+    A+ A- B+ B-
+    AB+ AB- B+ B-
+    A+ A- AB+ AB- B+ B- O+ O-
+    A+ AB+ B+ O+
+    AB+ B+
+    A+ A- O+ O-
+    A+ A- AB+ AB-
+    impossible
+    A+ A- AB+ AB- B+ B- O+ O-
+    A+ A- AB+ AB-
+    A+ A- AB+ AB-
+    A+ B+ O+
+    A+ A- O+ O-
+    A+ A- B+ B- O+ O-
+    A+ A- B+ B-
+    A+ AB+ B+ O+
+    A+ A- AB+ AB- B+ B- O+ O-
+    A+ A- AB+ AB- B+ B-
+    A+ A- AB+ AB- B+ B-
+    A+ A- B+ B- O+ O-
+    A+ A- AB+ AB- B+ B- O+ O-
+    A+ A- B+ B- O+ O-
+    AB+ AB- B+ B-
+    B+ B- O+ O-
+    AB+ AB- B+ B-
+    A- O-
+    impossible
+    B+ B- O+ O-
+    impossible
+    A+ A- B+ B-
+    A+ A- AB+ AB- B+ B- O+ O-
+    impossible
+    A+ A- AB+ AB- B+ B-
+    impossible
+    A+ AB+ B+ O+
+    A- AB- B-
+    AB+ B+
+    A+ A- AB+ AB- B+ B-
+    AB+ AB- B+ B-
+    AB+ AB- B+ B-
+    A+ A- AB+ AB-
+    A+ A- AB+ AB-
+    impossible
+    A+ A- AB+ AB-
+    A+ A- AB+ AB- B+ B- O+ O-
+    B+ B- O+ O-
+    B+ B- O+ O-
+    impossible
+    A+ A- AB+ AB-`;
+      expect(bloodTypes(input).toEqual(output));
+  });
+});
