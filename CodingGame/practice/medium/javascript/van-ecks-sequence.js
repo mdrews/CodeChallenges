02c86d52a62b0da594c2f31012f84574e1353104
mdrews <matthewdrews@gmail.com>
2020-06-17 23:30:59 -0400

    add van-ecks-sequence to CodinGame -> javascript -> medium

---
+++ CodingGame/practice/medium/javascript/van-ecks-sequence.js
@@ -0,0 +1,14 @@
+// https://www.codingame.com/ide/puzzle/van-ecks-sequence
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const A1 = parseInt(readline());
+const N = parseInt(readline());
+
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('answer');
